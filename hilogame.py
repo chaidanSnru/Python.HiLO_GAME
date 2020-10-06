a3540c71726bf3877c9e58ee27e8ccd26df9ca5a
chaidanSNRU <chaidan@snru.ac.th>
2020-10-06 16:28:56 +0700

    เกม hi lo เสร็จแล้ว แบบไม่ check input  ไม่เช็คจำนวนเงินที่แทง

--- hilogame.py
+++ hilogame.py
@@ -1,55 +1,57 @@
 import random
 
 playerMoney = 100
-prompt = '''ยินดีต้อนรับสู่ Hi-Lo เกม
-            คุณเลือก H หรือ L เพื่อพนัดในอัตรา 1 ต่อ 1
-            ถ้าลูกเต๋าออก 11 เจ้ามือชนะ
-            เริ่มต้นคุณจะมีทุนอยู่ 100 บาท เงินหมดเกมหยุด
-         '''
-quitMessage = "\nเลือก Q เมื่อต้องการออกจากเกม"
+prompt = "ยินดีต้อนรับสู่ Hi-Lo เกม\nนายเลือก H หรือ L เพื่อพนัดในอัตรา 1 ต่อ 1\nถ้าลูกเต๋าออก 11 เจ้ามือชนะ\nเริ่มต้นนายจะมีทุนอยู่ 100 บาท เงินหมดเกมหยุด"
+quitMessage = "\nเลือก Q เมื่อต้องการออกจากเกม "
 
 def diceRoll():
     diceFaces = range(1,7)
     return random.choice(diceFaces)
 
-def gambleResule(playerMoney, gamebleMoney, gambleChoice, firstDice,secondDice,thirdDice):
+def gambleResule(pMoney, gMoney, gChoice, fDice,sDice,thDice):
     dicesPoint = firstDice+secondDice+thirdDice
     if dicesPoint > 11:
-        print(f"Dices show {firstDice}-{secondDice}-{thirdDice}. Their point is {dicesPoint}")
-        print("It HI")
-        if gambleChoice == 'H':
-            print("You wins!!!")
-            playerMoney += int(gambleMoney)
-            return playerMoney
+        print(f"ลูกเต๋าออกมาเป็น {fDice}-{sDice}-{thDice}. รวมเป็น {dicesPoint}")
+        print("ออก สูง***")
+        if gChoice == 'H':
+            print("นาย ชนะ!!!")
+            pMoney += int(gMoney)
+            return pMoney
         else:
-            print("You Lost!!!")
-            playerMoney -= int(gambleMoney)
-            return playerMoney            
+            print("นาย แพ้!!!")
+            pMoney -= int(gMoney)
+            return pMoney            
     elif dicesPoint < 11:
-        print(f"Dices show {firstDice}-{secondDice}-{thirdDice}. Their point is {dicesPoint}")
-        print("It LO")
-        if gambleChoice == 'L':
-            print("You wins!!!")
-            playerMoney += int(gambleMoney)
-            return playerMoney
+        print(f"ลูกเต๋าออกมาเป็น {fDice}-{sDice}-{thDice}. รวมเป็น {dicesPoint}")
+        print("ออก ต่ำ***")
+        if gChoice == 'L':
+            print("นาย ชนะ!!!")
+            pMoney += int(gMoney)
+            return pMoney
         else:
-            print("You Lost!!!")
-            playerMoney -= int(gambleMoney)
-            return playerMoney   
+            print("นาย แพ้!!!")
+            pMoney -= int(gMoney)
+            return pMoney   
     else:
-        print("Hi-LO, dealer WIN !!!")
-        playerMoney -= int(gambleMoney)
-        return playerMoney
+        print("ออก ไฮโล 11 แต้ม เจ้ามือกินเรียบ นายแพ้***!!!")
+        pMoney -= int(gMoney)
+        return pMoney
 
 while playerMoney > 0:
     print(prompt)
-    print(f"Now, You have {playerMoney} Bath")
-    gambleMoney = input("How much Bath do you want to gamble ? ")
-    gambleChoice = input("What do you choice H/L ? ")
-    print(f"OK! you gamble for {playerMoney} Bath and you choosed {gambleChoice}, let rolls...")
+    print(f"ตอนนี้นายมีเงินอยู่ {playerMoney} บาท")
+    gambleMoney = input("นายต้องการแทงกี่บาท ? ")
+    gambleChoice = input("นายจะแทง สูง(H) หรือ ต่ำ(L) ? ")
+    print(f"ได้เลยยเพื่อน! นายแทงมา {gambleMoney} บาท และเลือก {gambleChoice} ซินะ, มา มา เขย่ากัน...")
     firstDice = diceRoll()
     secondDice = diceRoll()
     thirdDice = diceRoll()
     playerMoney = gambleResule(playerMoney, gambleMoney, gambleChoice, firstDice, secondDice, thirdDice)
-    print (f"Now you got {playerMoney} Bath")
+    print (f"ตอนนี้นายมี {playerMoney} บาท")
+    gamblePlay = input("นายต้องการเล่นไหม\nเลือก Q เมื่อต้องการออกจากเกม")
+    if gamblePlay == "Q":
+        break
+    else:
+        pass
+
 
